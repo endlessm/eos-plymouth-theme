f9384c43626c3584714dfa954fa394e0413778fa
Srdjan Grubor <sgnn7@sgnn7.org>
2014-02-05 15:19:23 -0600

    Updated boot test script to make it faster

--- tools/boot.sh
+++ tools/boot.sh
@@ -3,7 +3,7 @@
 sudo plymouthd --tty=`tty` --mode=boot --debug --debug-file=/tmp/plymouth-debug-out
 sudo plymouth --show-splash
 for ((I=0;I<10;I++)); do 
-  sleep 0.5
+  sleep 0.3
   sudo plymouth --update=event$I
 done
 sudo plymouth --quit
