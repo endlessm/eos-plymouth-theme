aa834726f777cc67e3ca0c8a97e1bc24022e5c2e
Srdjan Grubor <sgnn7@sgnn7.org>
2014-02-05 13:48:29 -0600

    Cleaned up the testing scripts a bit and added a new one

--- tools/boot.sh
+++ tools/boot.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 sudo plymouthd --tty=`tty` --mode=boot --debug --debug-file=/tmp/plymouth-debug-out
 sudo plymouth --show-splash
 for ((I=0;I<10;I++)); do 
-  sleep 0.5 
+  sleep 0.5
   sudo plymouth --update=event$I
 done
 sudo plymouth --quit
--- tools/fsck.sh
+++ tools/fsck.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+
 sudo plymouthd --tty=`tty` --mode=boot --debug --debug-file=/tmp/plymouth-debug-out
 sudo plymouth --show-splash
-for ((I=0;I<3;I++)); do 
-  sleep 0.5 
+for ((I=0;I<3;I++)); do
+  sleep 0.5
   sudo plymouth --update=event$I
 done
 
-for ((I=0;I<100;I++)); do 
-  sleep 0.01 
+for ((I=0;I<100;I++)); do
+  sleep 0.01
   sudo plymouth --update=fsck:sda1:$I
 done
 
-for ((I=4;I<10;I++)); do 
-  sleep 0.2 
+for ((I=4;I<10;I++)); do
+  sleep 0.2
   sudo plymouth --update=event$I
 done
 
